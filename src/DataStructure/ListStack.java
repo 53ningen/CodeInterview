3f5a6429f950441ef381dd5d70b9504670fc9678
53ningen <korun24276@gmail.com>
2014-06-10 03:13:53 +0900

    ArrayStackとListStackの基盤を実装

--- src/DataStructure/ListStack.java
+++ src/DataStructure/ListStack.java
@@ -4,7 +4,34 @@ package DataStructure;
  * Stack implementation
  * @param <E> type
  */
-public class Stack<E> {
+public class ListStack<E> {
+    private Node<E> top;
+    private int size = 0;
+
+    /**
+     * dataをスタックに挿入する
+     * @param data 挿入したいデータ
+     */
+    public void push(E data) {
+        Node<E> n = new Node<E>(data, top);
+        top = n;
+        size++;
+    }
+
+    /**
+     * スタックのトップにある要素を取り出す
+     * @return スタックのトップにあったデータ
+     */
+    public E pop() {
+        if(top == null)
+            return null;
+
+        E item = top.item;
+        top = top.next;
+        size--;
+        return item;
+    }
+
     /**
      * Stackのノード
      * @param <E> type
@@ -12,17 +39,23 @@ public class Stack<E> {
     class Node<E>{
         E item;
         Node<E> next;
-        java.util.Stack stack = new java.util.Stack();
         public Node(E item, Node<E> next) {
             this.item = item;
             this.next = next;
         }
     }
 
-    private Node<E> top;
-    private int size = 0;
-
-    public Stack(E... args){
+    public ListStack(E... args){
         // not implemented
+    }
+
+    /**
+     * Code Interview Chapter 3: Question 2
+     * pushとpopに加えて、最小の要素を返す関数minを持つスタックをどのようにデザインしますか？
+     * ただし、push, pop, min関数はすべてO(1)の実行時間になるようにしてください。
+     */
+    public Integer min(){
+        // your code goes here
+        return 0;
     }
 }
