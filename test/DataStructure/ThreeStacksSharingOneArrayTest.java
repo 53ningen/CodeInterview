d7d97a73bd05a8e567ac0865804d5aa316f4f69b
53ningen <korun24276@gmail.com>
2014-06-10 03:26:36 +0900

    Q.3-1のひな形を作成

---
+++ test/DataStructure/ThreeStacksSharingOneArrayTest.java
@@ -0,0 +1,13 @@
+package DataStructure;
+
+import org.junit.Test;
+
+/**
+ * テスト書かないとかそれ@t_wadaの前でも言えんの？
+ */
+public class ThreeStacksSharingOneArrayTest {
+    @Test
+    public void テスト() {
+
+    }
+}
