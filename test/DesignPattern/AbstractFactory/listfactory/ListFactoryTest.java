71193a12488fd39029ee949c07f39754b34f8742
53ningen <korun24276@gmail.com>
2014-07-01 16:49:59 +0900

    modify Abstract Factory Client Code

--- test/DesignPattern/AbstractFactory/listfactory/ListFactoryTest.java
+++ test/DesignPattern/AbstractFactory/listfactory/ListFactoryTest.java
@@ -1,19 +1,16 @@
 package DesignPattern.AbstractFactory.listfactory;
 
-import DesignPattern.AbstractFactory.factory.Factory;
-import DesignPattern.AbstractFactory.factory.Item;
-import DesignPattern.AbstractFactory.factory.Page;
-import DesignPattern.AbstractFactory.factory.Tray;
+import DesignPattern.AbstractFactory.factory.*;
 import org.junit.Test;
 
 public class ListFactoryTest {
     @Test
     public void Test() {
         Factory factory = Factory.getFactory("DesignPattern.AbstractFactory.listfactory.ListFactory");
-        Item asahi = factory.createLink("朝日新聞", "http://www.asahi.com/");
-        Item yomiuri = factory.createLink("読売新聞", "http://wwww.yomiuri.co.jp/");
-        Item us_yahoo = factory.createLink("Yahoo! UN", "http://www.yahoo.com/");
-        Item jp_yahoo = factory.createLink("Yahoo! Japan", "http://www.yahoo.co.jp");
+        Link asahi = factory.createLink("朝日新聞", "http://www.asahi.com/");
+        Link yomiuri = factory.createLink("読売新聞", "http://wwww.yomiuri.co.jp/");
+        Link us_yahoo = factory.createLink("Yahoo! UN", "http://www.yahoo.com/");
+        Link jp_yahoo = factory.createLink("Yahoo! Japan", "http://www.yahoo.co.jp");
 
         Tray traynews = factory.createTray("新聞");
         traynews.add(asahi);
