31beaa622b729b8b7b1d658688130cce95d24e36
53ningen <korun24276@gmail.com>
2014-07-10 14:11:44 +0900

    add Observer Pattern

---
+++ src/DesignPattern/Observer/DigitObserver.java
@@ -0,0 +1,13 @@
+package DesignPattern.Observer;
+
+public class DigitObserver implements Observer {
+    public void update(NumberGenerator generator) {
+        System.out.println("DigitObserver:" + generator.getNumber());
+        try {
+            Thread.sleep(100);
+        }
+        catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/DesignPattern/Observer/Observer.java
@@ -0,0 +1,5 @@
+package DesignPattern.Observer;
+
+public interface Observer {
+    public abstract void update(NumberGenerator generator);
+}
