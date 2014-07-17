bcc99122bd0a40e43830f0ad39e73e5442e0bb8d
Yuki Yanagi <yuki_yanagi@dwango.co.jp>
2014-07-17 11:34:42 +0900

    Update Queue.java

--- src/DataStructure/queue/Queue.java
+++ src/DataStructure/queue/Queue.java
@@ -3,6 +3,7 @@ package DataStructure.queue;
 public abstract class Queue<E> {
     public abstract void enqueue(E e);
     public abstract E dequeue();
+    public abstract boolean isEmpty();
     public int size() {
         throw new UnsupportedOperationException();
     }
